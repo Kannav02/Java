ca148cc01fc19b853c2f61425105ebb297a1dfa5
Kannav Sethi <kannavsethi2@gmail.com>
2021-12-22 00:20:09 +0530

    Bubble sort file added

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+
+public class BubbleSort{
+    public static void main(String[] args){
+        int[] arr={0,3,41,2,38,6,9,3,2};
+        // Bubble Sorting Mechanism
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[]arr){
+        int n =arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
