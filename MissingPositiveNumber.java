e010037ab2d0acf1b792b897b93bf4c6c24566a0
Kannav Sethi <kannavsethi2@gmail.com>
2021-12-24 21:28:51 +0530

    File for finding missing positive number has been added

---
+++ MissingPositiveNumber.java
@@ -0,0 +1,29 @@
+public class MissingPositiveNumber {
+    public static void main(String[] args){
+
+    }
+    static int sort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if( arr[i]!=arr[correct] ){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+    
+        for(int index=0;index<arr.length;index++){
+            if(arr[index]!=index+1 && arr[index]>0){
+                return index+1;
+            }
+        }
+        return -1;
+    }
+    static void swap(int []arr,int start, int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+}
