64eef34579e02d490844f6fd6b5aa53998c28fc6
Kannav Sethi <kannavsethi2@gmail.com>
2021-12-24 23:49:00 +0530

    Changes made to the programme

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,12 +1,14 @@
 public class MissingNumber {
     public static void main(String[] args){
+        int[]arr={1,2,3,4};
+        System.out.println(sort(arr));
 
     }
     static int sort(int[] arr){
         int i=0;
         while(i<arr.length){
-            int correct=arr[i];
-            if(arr[i]>0 &&arr[i]<arr.length && arr[i]!=arr[correct] ){
+            int correct=arr[i]-1;
+            if(arr[i]>0 && arr[i]<=arr.length && arr[i]!=arr[correct] ){
                 swap(arr,i,correct);
             }
             else{
@@ -15,11 +17,11 @@ public class MissingNumber {
         }
     
         for(int index=0;index<arr.length;index++){
-            if(arr[index]!=index){
-                return index;
+            if(arr[index]!=index+1){
+                return index+1;
             }
         }
-        return arr.length;
+        return arr.length+1;
     }
     static void swap(int []arr,int start, int end){
         int temp=arr[start];
