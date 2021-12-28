51880f5907f22a08ea35f84d4cae4588e24df4bf
Kannav Sethi <kannavsethi2@gmail.com>
2021-12-29 01:07:21 +0530

    the file for fanged string leetcode question has been added

---
+++ FangedString.java
@@ -0,0 +1,13 @@
+public class FangedString {
+    public static void main( String [] args){
+        // programme to swap the element with the element you want 
+
+    }
+    static String ans(String a){
+        // here we just use the inbuilt string function that is replace which will just only replace all the occurences of this element with the element you want
+        String b=a.replace(".", "[.]");
+        return b;
+
+    }
+    
+}
