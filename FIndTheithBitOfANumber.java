3c6ab3e17853d21e14a5bc8d977df7738f10bb1b
Kannav Sethi <kannavsethi2@gmail.com>
2021-12-30 19:23:06 +0530

    Changes made

--- FIndTheithBitOfANumber.java
+++ FIndTheithBitOfANumber.java
@@ -1,10 +1,10 @@
 public class FIndTheithBitOfANumber {
     public static void main(String[]args){
     System.out.println(Integer.toBinaryString(45));
-    System.out.print(Integer.toBinaryString(find(45,3)));
+    System.out.print(Integer.toBinaryString(find(45,4)));
 }
     static int find(int n,int i){
-        return((n&(1<<i-1)));
+        return((n&(1<<i-1))>>i-1);
         }
     }
     
